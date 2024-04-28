36bfd2bb725aa7cf0a408a8fc716c44793301f0b
KuzmaJn <jankuzma.810@gmail.com>
2024-04-28 15:37:16 +0200

    added AgencyInterface and implementation

---
+++ src/main/java/sk/stuba/fei/uim/oop/entity/grant/Agency.java
@@ -0,0 +1,43 @@
+package sk.stuba.fei.uim.oop.entity.grant;
+
+import java.util.*;
+
+public class Agency implements AgencyInterface{
+    private String name;
+    private Map<GrantInterface, Integer> grants = new HashMap<>();
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void addGrant(GrantInterface gi, int year) {
+        grants.put(gi, year);
+    }
+
+
+    @Override
+    public Set<GrantInterface> getAllGrants() {
+        return new HashSet<>(grants.keySet());
+    }
+
+    /**
+     * Mak
+     */
+    @Override
+    public Set<GrantInterface> getGrantsIssuedInYear(int year) {
+        Set<GrantInterface> grantsInYear = new HashSet<>();
+        for(Map.Entry<GrantInterface, Integer> entry : grants.entrySet()) {
+            if (entry.getValue() == year) {
+                grantsInYear.add(entry.getKey());
+            }
+        }
+        return grantsInYear;
+    }
+}
